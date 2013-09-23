e76f845d3d800e2bbecb774a480d2ba4bc7a53fb
Ilya Levin <almaron@gmail.com>
2013-09-23 21:59:40 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Placemapper::Application.config.secret_key_base = 'd9b76c68820a9ccfd15900d2760d1c4e123ee6a0efa0834251d24c7a26dcce286ffafe09a2d4410ba4bc7de40d2e6d1e97751408ff81f4f1caa879acfb4527a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Placemapper::Application.config.session_store :cookie_store, key: '_placemapper_session'
